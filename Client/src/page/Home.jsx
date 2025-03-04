96d99b0019c53e4f8a02bebdc2e7e734372cd4ee
Paul <povilas.geguzis@gmail.com>
2025-03-04 13:08:48 +0400

    started category buttons

--- Client/src/page/Home.jsx
+++ Client/src/page/Home.jsx
@@ -1,21 +1,34 @@
 import heroBanner from "../assets/HeroBanner.png";
+import Button from "../components/button";
 
 
 const Home = () => {
 
     return (
-        <>
+        <div>
             <div className="relative w-full h-[37.5rem]">
-                <section className="absolute w-full h-full" style={{ backgroundImage: `url(${heroBanner})` }}>
-                    <div className="m-20">
-                        <h1 className="text-wh">Connect, Create, Celebrate</h1>
+                <section className="absolute w-full h-full bg-cover bg-bottom" style={{ backgroundImage: `url(${heroBanner})` }}></section >
+                <section className="bg-black/50 w-full h-full absolute">
+                    <div className="mx-20 w-[42rem] flex flex-col gap-8">
+                        <h1 className="text-white text-heading-xl mt-48 font-[700]">Connect, Create, Celebrate</h1>
+                        <p className="text-white text-heading-s/tight font-[400]">
+                            Discover amazing events or create your own.
+                            Join a comminty of people who love to connect and share experiences.
+                        </p>
+                        <div className="flex gap-4">
+                            <Button >Create Event</Button>
+                            <Button background="bg-white" textColor="text-btn">Join Event</Button>
+                        </div>
                     </div>
-                </section >
-                <section className="bg-black/50 w-full h-full absolute"></section>
+                </section>
+            </div>
+            <div className="text-center py-16 px-20">
+                <h1 className="text-heading-l">Explore Events By Category</h1>
+                <CategoryButton></CategoryButton>
+
             </div>
 
-
-        </>
+        </div>
     )
 }
 export default Home;
