968c2f25dff04c7c50fe308d7fd1f54a3c29db79
Paul <povilas.geguzis@gmail.com>
2025-03-05 08:09:05 +0200

    fix mobile layout

--- Client/src/components/CategoryButton.jsx
+++ Client/src/components/CategoryButton.jsx
@@ -1,16 +1,18 @@
 const CategoryButton = ({
   children,
   onClick,
-  type = "button",
-  styleType = "primary",
-  size = "big",
-  background="bg-[#FEFCE8]",
-  textColor="text-white"
+  type = 'button',
+  styleType = 'primary',
+  size = 'big',
+  background = 'bg-[#FEFCE8]',
+  textColor = 'text-white',
+  picture,
+  text
 }) => {
   const sizes = {
-    big: "h-[6.75rem] w-[18.375rem]",
-    medium: "h-[5.5rem] w-[12rem]",
-    small: "h-[4.5rem] w-[8rem]",
+    big: 'h-[6.75rem] w-[18.375rem]',
+    medium: 'h-[5.5rem] w-[12rem]',
+    small: 'h-[4.5rem] w-[8rem]',
   };
 
   return (
@@ -19,7 +21,10 @@ const CategoryButton = ({
       onClick={onClick}
       className={`${textColor} px-4 ${background} ${sizes[size]} text-4 not-italic font-[400] rounded-[0.5rem] btn btn-soft hover:bg-[#fcf6b7] hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] border-0 desktop:h-[6.75rem] desktop:w-[18.375rem] tablet:h-[5.5rem] tablet:w-[11rem] h-[6.75rem] w-[18.375rem]`}
     >
-      {children}
+      <div className="text-black font-[600] flex flex-col items-center">
+        <img src={picture} alt="music Icon" />
+        <p className="mb-[0.25rem]">{text}</p>
+      </div>
     </button>
   );
 };
--- Client/src/page/Home.jsx
+++ Client/src/page/Home.jsx
@@ -1,25 +1,26 @@
 import heroBanner from '../assets/HeroBanner.png';
 import Button from '../components/button';
-import CategoryButton from '../components/CategoryButton';
 import boardgamesIcon from '../assets/Boardgames-category.svg';
 import musicIcon from '../assets/music-category.svg';
 import WorkshopIcon from '../assets/workshop-category.svg';
 import SportsIcon from '../assets/sports-category.svg';
+import CategoryButton from '../components/CategoryButton';
 
 const Home = () => {
   return (
     <div>
-      <div className="relative w-full h-[37.5rem]">
+      <div className="relative w-full h-150">
         <section
           className="absolute w-full h-full bg-cover bg-bottom"
           style={{ backgroundImage: `url(${heroBanner})` }}
         ></section>
+
         <section className="bg-black/50 w-full h-full absolute">
-          <div className="mx-20 w-[42rem] flex flex-col gap-8">
-            <h1 className="text-white text-heading-xl mt-48 font-[700]">
+          <div className="mx-20 max-w-168 flex flex-col gap-8">
+            <h1 className="text-white min-w-140 text-heading-xl mt-48 font-[700]">
               Connect, Create, Celebrate
             </h1>
-            <p className="text-white text-heading-s/tight font-[400] text-wrap">
+            <p className="text-white min-w-96 text-heading-s/tight font-[400]">
               Discover amazing events or create your own. Join a comminty of
               people who love to connect and share experiences.
             </p>
@@ -38,41 +39,21 @@ const Home = () => {
         </section>
       </div>
 
-      <div className="flex flex-col items-center justify-between py-16 px-20 gap-y-12 w-full ">
+      <div className="flex flex-col items-center justify-between py-16 px-20 gap-y-12 w-full bg-white">
         <h1 className="text-heading-l text-black font-[700] flex flex-col ">
           Explore Events By Category
         </h1>
 
         <div className="flex gap-x-6 gap-y-6 tablet:gap-y-0 flex-col items-center tablet:flex-row justify-center">
-          <CategoryButton>
-            <div className="text-black font-[600] flex flex-col items-center">
-              <img src={musicIcon} alt="music Icon" />
-              <p className="mb-[0.25rem]">Music</p>
-            </div>
-          </CategoryButton>
-          <CategoryButton>
-            <div className="text-black font-[600] flex flex-col items-center">
-              <img src={SportsIcon} alt="Sports Icon" />
-              <p className="mb-[0.25rem]">Sports</p>
-            </div>
-          </CategoryButton>
-          <CategoryButton>
-            <div className="text-black font-[600] flex flex-col items-center">
-              <img src={WorkshopIcon} alt="Workshop Icon" />
-              <p className="mb-[0.25rem]">Music</p>
-            </div>
-          </CategoryButton>
-          <CategoryButton>
-            <div className="text-black font-[600] flex flex-col items-center">
-              <img src={boardgamesIcon} alt="boardgames Icon" />
-              <p className="mb-[0.25rem]">Board Games</p>
-            </div>
-          </CategoryButton>
+          <CategoryButton text={"Music"} picture={musicIcon}/>
+          <CategoryButton text={"Sports"} picture={SportsIcon}/>
+          <CategoryButton text={"Workshop"} picture={WorkshopIcon}/>
+          <CategoryButton text={"Board Games"} picture={boardgamesIcon}/>
         </div>
       </div>
 
-      <div className="flex flex-col mx-24 justify-center">
-        <h2 className="text-heading-m">Recommended for You</h2>
+      <div className="flex flex-col px-24 justify-center bg-light-gray">
+        <h2 className="text-heading-m mt-8 mb-6">Recommended for You</h2>
         <p className="text-center">--------KORTELIU PLACEHOLDER---------</p>
       </div>
     </div>
