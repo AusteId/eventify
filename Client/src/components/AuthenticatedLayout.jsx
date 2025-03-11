bbf5d4b77ec8c0c2149991b1a985aac41bd35c60
Paul <povilas.geguzis@gmail.com>
2025-03-11 16:52:57 +0200

    minor fix to button components

--- Client/src/components/AuthenticatedLayout.jsx
+++ Client/src/components/AuthenticatedLayout.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router';
 import Header from './Header/Header'
-import Footer from './Footer';
 import NotSignedInHeader from './Header/NotSignedInHeader';
 
 const AuthenticatedLayout = ({ authenticationStatusPlaceholder = true }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-full flex flex-col">
       {authenticationStatusPlaceholder && <Header />}
       {!authenticationStatusPlaceholder && <NotSignedInHeader />}
       <div className="flex-1">
