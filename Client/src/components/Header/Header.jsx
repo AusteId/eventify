3afb208a6de907e9820286c93773555729a59e9d
lukas622 <bsds356@proton.me>
2025-03-13 14:51:55 +0200

    Revert header.jsx

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -45,15 +45,7 @@ const Header = () => {
           </section>
         </section>
         <section className=" h-[2.5rem]  flex justify-center items-center gap-[1rem] ">
-          {/* <Button>Create Event</Button> */}
-          <button
-            onClick={() =>
-              document.getElementById('event_creation_modal').showModal()
-            }
-            className="btn bg-btn border-0 shadow-none hover:bg-btn-hover px-4 pt-2 pb-2 rounded-lg"
-          >
-            Create Event
-          </button>
+          <Button>Create Event</Button>
           <BasicModal id="event_creation_modal">
             <CreateEventForm />
           </BasicModal>
