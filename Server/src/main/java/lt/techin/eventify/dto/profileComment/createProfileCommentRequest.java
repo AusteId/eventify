6e76bc117c1dbefe15c3afa017cb38061a408a77
lukas622 <bsds356@proton.me>
2025-03-19 23:09:54 +0200

    Small change

--- Server/src/main/java/lt/techin/eventify/dto/profileComment/createProfileCommentRequest.java
+++ Server/src/main/java/lt/techin/eventify/dto/profileComment/createProfileCommentRequest.java
@@ -2,15 +2,16 @@ package lt.techin.eventify.dto.profileComment;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lt.techin.eventify.model.User;
 
 import java.sql.Timestamp;
 
 public record createProfileCommentRequest(
         @NotNull(message =  "Commenter cannot be null")
-        long commenterId,
+        User commenterId,
 
         @NotNull(message =  "Commented cannot be null")
-        long commentedId,
+        User commented,
 
         @NotNull(message =  "Message cannot be null")
         @Size(max = 1000, message = "Message cannot exceed 1000 characters.")
