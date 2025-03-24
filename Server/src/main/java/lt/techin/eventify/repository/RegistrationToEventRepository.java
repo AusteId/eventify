d2f37616eace6d86008ec027a552be06ebb5f4f2
Darius <juchevicd@gmail.com>
2025-03-24 15:04:04 +0400

    Add new method to registration repository

--- Server/src/main/java/lt/techin/eventify/repository/RegistrationToEventRepository.java
+++ Server/src/main/java/lt/techin/eventify/repository/RegistrationToEventRepository.java
@@ -1,9 +1,13 @@
 package lt.techin.eventify.repository;
 
 import lt.techin.eventify.model.RegistrationToEvent;
+import lt.techin.eventify.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface RegistrationToEventRepository extends JpaRepository<RegistrationToEvent, Long> {
   int countByEventId(Long id);
   boolean existsByUserIdAndEventId(Long userId, Long eventId);
+  List<User> findAllByEventId(Long eventId);
 }
