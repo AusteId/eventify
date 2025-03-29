cf8f0eeded156b9bab3e9b0c8c0d3dff32fee2df
lukas622 <bsds356@proton.me>
2025-03-29 03:52:24 +0200

    Hero section, header

---
+++ Client/src/components/HeroSection.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import heroBanner from '../assets/hero.jpg';
+import { useAuth } from '../components/Auth/AuthContext';
+import Button from './Button';
+
+const HeroSection = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+  return (
+    <div className="relative h-[calc(100vh-72px)] w-full">
+      <section
+        className="absolute w-full h-full bg-cover bg-bottom"
+        style={{ backgroundImage: `url(${heroBanner})` }}
+      ></section>
+      <section className="bg-black/50 w-full h-full absolute content-center text-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-white min-w-140 text-heading-xxl font-[700]">
+            Connect, Create, Celebrate
+          </h1>
+          <p className="text-white min-w-96 text-heading-s/tight font-[400] mt-4">
+            Discover amazing events or create your own. Join a comminty of
+            people who love to connect and share experiences.
+          </p>
+          <div className="flex gap-4 mt-16">
+            <Button
+              onClick={() => {
+                if (isAuthenticated) {
+                  document.getElementById('event_creation_modal').showModal();
+                } else {
+                  navigate('/login');
+                }
+              }}
+              size="big"
+            >
+              Create Event
+            </Button>
+            <Button
+              size="big"
+              background="bg-white"
+              textColor="text-btn"
+              hoverColor="hover:bg-[#fcf6b7]"
+              onClick={() => {
+                navigate('/events');
+              }}
+            >
+              Join Event
+            </Button>
+          </div>
+        </div>
+        {/* <div className="absolute left-1/2 transform -translate-x-1/2 bottom-1/8 flex flex-col items-center">
+          <button
+            className="flex flex-col items-center justify-center text-white animate-bounce cursor-pointer focus:outline-none  rounded-full p-2"
+            aria-label="Scroll down to explore events"
+          >
+            <span className="text-heading-s mb-1">Explore</span>
+            <ChevronDown size={35} />
+          </button>
+        </div> */}
+      </section>
+    </div>
+  );
+};
+
+export default HeroSection;
--- Client/src/page/Home.jsx
+++ Client/src/page/Home.jsx
@@ -1,13 +1,12 @@
 import { useNavigate } from 'react-router';
 import boardgamesIcon from '../assets/Boardgames-category.svg';
-import heroBanner from '../assets/HeroBanner.png';
 import musicIcon from '../assets/music-category.svg';
 import SportsIcon from '../assets/sports-category.svg';
 import WorkshopIcon from '../assets/workshop-category.svg';
 import { useAuth } from '../components/Auth/AuthContext';
-import Button from '../components/Button';
 import CategoryButton from '../components/CategoryButton';
 import EventCarousel from '../components/EventCarousel';
+import HeroSection from '../components/HeroSection';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -15,7 +14,8 @@ const Home = () => {
 
   return (
     <div>
-      <div className="relative w-full h-150">
+      <HeroSection />
+      {/* <div className="relative w-full h-150">
         <section
           className="absolute w-full h-full bg-cover bg-bottom"
           style={{ backgroundImage: `url(${heroBanner})` }}
@@ -57,7 +57,7 @@ const Home = () => {
             </div>
           </div>
         </section>
-      </div>
+      </div> */}
 
       <div className="flex flex-col items-center justify-between py-16 px-20 gap-y-12 w-full bg-white">
         <h1 className="text-heading-l text-black font-[700] flex flex-col ">
