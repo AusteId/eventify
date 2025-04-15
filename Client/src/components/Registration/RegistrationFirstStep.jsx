dd13c071efcabe3609c5f740da4136366d474190
lukas622 <bsds356@proton.me>
2025-04-15 15:03:49 +0300

    Design progress

--- Client/src/components/Registration/RegistrationFirstStep.jsx
+++ Client/src/components/Registration/RegistrationFirstStep.jsx
@@ -9,6 +9,7 @@ import { useNotifications } from '../context/NotificationContext';
 import FieldValidationError from '../FieldValidationError';
 import LoadingScreen from '../message/LoadingScreen';
 import StepIndicator from '../StepIndicator';
+import RegistrationSteps from '../RegistrationSteps';
 
 const RegistrationFirstStep = forwardRef((props, ref) => {
   const [passwordMatchError, setPasswordMatchError] = useState('');
@@ -136,6 +137,7 @@ const RegistrationFirstStep = forwardRef((props, ref) => {
   return (
     <>
       {isLoading && <LoadingScreen />}
+      <RegistrationSteps />
       <div className="flex flex-col gap-8  mt-[3rem] bg-white rounded-2xl shadow-md px-9 pt-8 pb-12">
         <div>
           <h1 className="font-bold text-black text-center text-heading-m/normal mb-12">
@@ -266,7 +268,10 @@ const RegistrationFirstStep = forwardRef((props, ref) => {
             <p className="font-inter text-body-medium">
               Already have an account?
             </p>
-            <a className="text-btn-hover cursor-pointer" onClick={() => navigate("/login")}>
+            <a
+              className="text-btn-hover cursor-pointer"
+              onClick={() => navigate('/login')}
+            >
               Sign in
             </a>
           </div>
---
+++ Client/src/components/RegistrationSteps.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RegistrationSteps = () => {
+  return (
+    <div className="flex gap-20 relative items-center">
+      <div className="absolute w-full h-1 bg-slate-400 z-0"></div>
+      <div className="w-20 h-20 rounded-full bg-btn z-1"></div>
+      <div className="w-20 h-20 rounded-full bg-btn z-1"></div>
+      <div className="w-20 h-20 rounded-full bg-btn z-1"></div>
+      <div className="w-20 h-20 rounded-full bg-btn z-1"></div>
+    </div>
+  );
+};
+
+export default RegistrationSteps;
