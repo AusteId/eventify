fde0beece9aee8e74486fa05868963e087f65269
D2JP <d2jp@TECHIN.INTERNAL>
2025-04-18 09:47:23 +0300

    Add Organizer card component

---
+++ Client/src/components/OrganizerCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const OrganizerCard = ({ image, title, rating, description }) => {
+  return (
+    <div className="">
+      <div
+        className="h-31 @apply w-[18.375rem] flex-col justify-center shadow-[0px_2px_4px_0px_rgba(0,0,0,0.10),0px_4px_6px_0px_rgba(0,0,0,0.10)] p-4 rounded-xl border-0 border-solid;
+        background: #fff; flex"
+      >
+        <div className="flex w-full">
+          <div className=" bg-gray-300 flex flex-row mb-2 w-12 h-12 rounded-full object-cover gap-3">
+            <img
+              className="flex flex-row bg-gray-300 rounded-full"
+              src={image}
+            />
+            <div className="gap-y-2 flex flex-col justify-start">
+              <h3 className="whitespace-nowrap @apply text-gray-800 text-base not-italic font-bold leading-4 flex font-family: Inter;">
+                {title}
+              </h3>
+
+              <span className="gap-1 text-sm flex">
+                {' '}
+                <svg
+                  className="w-4 h-4 text-yellow-400"
+                  fill="currentColor"
+                  viewBox=" 0 0 20 20"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.97a1 1 0 00.95.69h4.15c.969 0 1.371 1.24.588 1.81l-3.357 2.44a1 1 0 00-.364 1.118l1.287 3.97c.3.921-.755 1.688-1.54 1.118l-3.357-2.44a1 1 0 00-1.175 0l-3.357 2.44c-.784.57-1.839-.197-1.54-1.118l1.287-3.97a1 1 0 00-.364-1.118L2.314 9.397c-.783-.57-.38-1.81.588-1.81h4.15a1 1 0 00.95-.69l1.286-3.97z" />
+                </svg>
+                {rating}
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between w-full flex-col pb-1"></div>
+
+        <p className="text-sm flex  text-gray-600/70 ">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default OrganizerCard;
--- Client/src/page/Home.jsx
+++ Client/src/page/Home.jsx
@@ -7,10 +7,42 @@ import { useAuth } from '../components/Auth/AuthContext';
 import CategoryButton from '../components/CategoryButton';
 import EventCarousel from '../components/EventCarousel';
 import HeroSection from '../components/HeroSection';
+import OrganizerCard from '../components/OrganizerCard';
 
 const Home = () => {
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth();
+
+  const cards = [
+    {
+      image:
+        'https://th.bing.com/th/id/R.3d5271def48f4fdb163015c4da4d6e16?rik=l%2bhuF9bJ%2bJJgkQ&riu=http%3a%2f%2fesct2019.storage.googleapis.com%2fwp-content%2fuploads%2f2020%2f01%2fwsi-imageoptim-tom.jpg%3fx80666&ehk=akLDM0Q0xk2sPaJZ76q%2fztoCrl%2fiNEmnEa6fZ%2fMYZiI%3d&risl=&pid=ImgRaw&r=0',
+      title: 'Sports Club',
+      rating: 4.8,
+      description: 'Organizing sports events since 2020',
+    },
+    {
+      image:
+        'https://th.bing.com/th/id/R.5e6d445e63ce55041a8c5ad887717f58?rik=XXsZ8BSNCZHfDA&pid=ImgRaw&r=0',
+      title: 'Art Gallery',
+      rating: 4.7,
+      description: 'Weekly art exhibitions and worksho...',
+    },
+    {
+      image:
+        'https://cdn.pixabay.com/photo/2023/12/02/10/40/ai-generated-8425436_640.jpg',
+      title: 'Beach Party',
+      rating: 4.7,
+      description: 'Host of the most wild parties',
+    },
+    {
+      image:
+        'https://bgr.com/wp-content/uploads/2019/02/download-1.jpeg?resize=300',
+      title: 'Movie night',
+      rating: 4.5,
+      description: 'Organizing events to relax with frien...',
+    },
+  ];
 
   const handleCategoryClick = category => {
     navigate(`/events?category=${encodeURIComponent(category.toLowerCase())}`);
@@ -73,6 +105,23 @@ const Home = () => {
         fetchUrl={`${import.meta.env.VITE_BACK_URL}/api/events/upcoming`}
         title={'Recommended for You'}
       /> */}
+
+      <h1 className="items-center text-heading-l text-header-dark font-bold p-[14%] py-16  gap-y-12 w-full @apply flex flex-col justify-center gap-4 shadow-[0px_2px_4px_0px_rgba(0,0,0,0.10),0px_4px_6px_0px_rgba(0,0,0,0.10)]">
+        <div className="pr-[45%] items-start">
+          Events from Popular Organizers
+        </div>
+        <div className=" items-center flex flex-wrap justify-center gap-8">
+          {cards.map((card, index) => (
+            <OrganizerCard
+              key={index}
+              image={card?.image}
+              title={card?.title}
+              rating={card.rating}
+              description={card.description}
+            />
+          ))}
+        </div>
+      </h1>
     </div>
   );
 };
